4127cc24c8edbaee3788c8bae8ef6323425af0e8
Shruthilaya Jindamwar <jindamwarshruthilaya@gmail.com>
2026-02-23 06:02:53 +0530

    variables

---
+++ 1-variables.js
@@ -0,0 +1,20 @@
+// =================
+
+console.log('Shru');
+
+// =================
+
+var name1 = 'Ramu'; // old way
+console.log(name1);
+
+let name2 = 'Sham'; // new way
+console.log(name2);
+
+// =================
+
+const name3 = 'Venu';
+console.log(name3);
+
+// name3 = 'Venu2';
+
+// =================
