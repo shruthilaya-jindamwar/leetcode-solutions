0fcf22612b560541a14faed91a652e7d5989071c
Shruthilaya Jindamwar <jindamwarshruthilaya@gmail.com>
2026-02-23 06:19:43 +0530

    js types

---
+++ 2-types.js
@@ -0,0 +1,46 @@
+
+// Number
+let rollNum = 123
+console.log(rollNum)
+// 123
+
+
+// String
+let studentName = 'Shru Jind'
+console.log(studentName)
+// Shru Jind
+
+// Boolean
+let isTakingCoaching = true
+console.log(isTakingCoaching)
+// true
+
+// ===========
+
+// Object  ( key, value pairs )
+let student = {
+    rollNum: 123,
+    studentName: 'Shru Jind',
+    isTakingCoaching: true
+}
+console.log(student)
+// { rollNum: 123, studentName: 'Shru Jind', isTakingCoaching: true }
+
+
+// ===========
+
+// Array of numbers
+let marks  = [ 100, 90, 50,  36 ]
+console.log(marks)
+// [ 100, 90, 50, 36 ]
+
+// Array of strings
+let names  = [ 'Sru', 'Ram', 'Lax' ]
+console.log(names)
+// [ 'Sru', 'Ram', 'Lax' ]
+
+let passedArray = [ true, false]
+console.log(passedArray)
+// [ true, false ]
+
+// ===========
